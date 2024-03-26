29de6f428f1cc9f5cf2e43f391e2348e623dff3d
Gaurav <gauravsinghkhon@gmail.com>
2024-03-26 16:58:41 +0530

    Added Feign Framework, to use other microservices

--- src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.example.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,9 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyExchangeProxy proxy;
 
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
@@ -22,6 +26,16 @@ public class CurrencyConversionController {
 		ResponseEntity<CurrencyConversion> responseEntity=new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversion.class, uriVariables);
 		
 		CurrencyConversion currencyConversion=responseEntity.getBody(); 
+		return new CurrencyConversion(currencyConversion.getId(),from,to,quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
+	}
+	
+
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
+		
+		
+		
+		CurrencyConversion currencyConversion=proxy.retrieveExchangeValue(from, to);
 		return new CurrencyConversion(currencyConversion.getId(),from,to,quantity,currencyConversion.getConversionMultiple(),quantity.multiply(currencyConversion.getConversionMultiple()),currencyConversion.getEnvironment());
 	}
 }

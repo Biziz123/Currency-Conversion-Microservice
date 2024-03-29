31859e993933c56409be27ae356a099b7d55dde9
Gaurav <gauravsinghkhon@gmail.com>
2024-03-29 19:21:29 +0530

    Added Eureka naming server

--- src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 
-@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
